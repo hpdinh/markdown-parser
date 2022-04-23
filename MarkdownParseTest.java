65bd36c2c0cd9b8f32e8d9fc7f27e041bf2e13b5
hpdinh <haydenpdinh@gmail.com>
2022-04-22 18:21:02 -0700

    Trying to fix junit

---
+++ MarkdownParseTest.java
@@ -0,0 +1,11 @@
+import java.beans.Transient;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    publc void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
