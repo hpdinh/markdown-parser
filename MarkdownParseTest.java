fc5fb465f6aae0b49d5a69efbf6b918501552162
hpdinh <haydenpdinh@gmail.com>
2022-04-22 18:29:05 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,9 @@
-import java.beans.Transient;
-
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
-    publc void addition() {
+    public void addition() {
         assertEquals(2, 1 + 1);
     }
 }
