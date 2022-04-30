1f3d4bd9cddc408f1d5a6daf18a759f27103fe94
hpdinh <haydenpdinh@gmail.com>
2022-04-29 19:22:59 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -24,7 +24,13 @@ public class MarkdownParseTest {
     }
     
     @Test
-    public void fail() {
-        assertEquals(4, 2+2);
+    public void imageTest() throws IOException{
+        ArrayList<String> expected = new ArrayList();
+        Path fileName = Path.of("test-file6.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(expected, links);
     }
+
+
 }
